001df7be70e44d19000aaa67736d0b227f6303f4
kirubadharshini <124122603+kirubadharshini@users.noreply.github.com>
2023-07-13 13:02:43 +0530

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,14 @@
+
+pipeline {
+    agent any
+    stages {
+        stage("Build changereq") {
+            when {
+                changeset glob: *.js*
+            }
+            steps {
+                echo "hi"
+            }
+        }
+    }
+}
